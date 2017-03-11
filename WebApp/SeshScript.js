2c3a9283c6093218d9c704593d9209feb3700995
Chris <cr217@st-andrews.ac.uk>
2017-03-11 18:24:24 +0000

    adding button to html and javascript

--- WebApp/SeshScript.js
+++ WebApp/SeshScript.js
@@ -1,6 +1,10 @@
 /**
  * Created by Chris on 11/03/2017.
  */
+var StartingBalance;
+var SpendingLimit;
+var DrinkCount;
+
 (function($) {
     $.fn.currencyInput = function() {
         this.each(function() {
@@ -25,26 +29,8 @@ $(document).ready(function() {
     $('input.currency').currencyInput();
 });
 
-(function($) {
-    $.fn.currencyInput = function() {
-        this.each(function() {
-            var wrapper = $("<div class='currency-input' />");
-            $(this).wrap(wrapper);
-            $(this).before("<span class='currency-symbol'>$</span>");
-            $(this).change(function() {
-                var min = parseFloat($(this).attr("min"));
-                var max = parseFloat($(this).attr("max"));
-                var value = this.valueAsNumber;
-                if(value < min)
-                    value = min;
-                else if(value > max)
-                    value = max;
-                $(this).val(value.toFixed(2));
-            });
-        });
-    };
-})(jQuery);
-
-$(document).ready(function() {
-    $('input.currency').currencyInput();
+$(document).ready(function(){
+    $('#SeshStartButton').click(function(){
+        document.body.appendChild(canvas);
+    });
 });
